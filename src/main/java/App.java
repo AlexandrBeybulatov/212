417b8b5399c8ced60156a27e6687f66be019f1cd
AlexandrBeybulatov <53230560+AlexandrBeybulatov@users.noreply.github.com>
2021-04-14 08:48:34 +0200

    rev

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat one = applicationContext.getBean(Cat.class);
+        Cat two = applicationContext.getBean(Cat.class);
+        System.out.println(one.getName());
+        System.out.println(bean == bean1);
+        System.out.println(one == two);
+    }
+}
